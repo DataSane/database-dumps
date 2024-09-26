5266afb66f87262402725ac4862d7e5aec03b756
jeancharlesx64 <jean.albuquerque@sptech.school>
2024-09-26 09:47:47 -0300

    [INIT] :: first base-database structure

---
+++ script/insert-base.sql
@@ -0,0 +1,4 @@
+USE datasane;
+
+INSERT INTO user VALUES
+	(DEFAULT, 'admin', 'admin@datasane.com','toor');
---
+++ script/structure.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE datasane;
+
+USE datasane;
+
+CREATE TABLE user(
+	userid INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(80),
+    email VARCHAR(200),
+    password VARCHAR(2555)
+);
